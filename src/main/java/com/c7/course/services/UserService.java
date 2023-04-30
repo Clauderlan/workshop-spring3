acf42bac66c50dca133aadde98d659052d126be3
Clauderlan <clauderlanbatista@hotmail.com>
2023-04-30 14:00:10 -0300

    User update

--- src/main/java/com/c7/course/services/UserService.java
+++ src/main/java/com/c7/course/services/UserService.java
@@ -5,6 +5,7 @@ import com.c7.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.parser.Entity;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,8 +27,19 @@ public class UserService {
     public User insert(User obj){
         return userRepository.save(obj);
     }
-
     public void delete(Long id){
         userRepository.deleteById(id);
+    }
+    public User update(Long id, User obj){
+        User entity = userRepository.findById(id).get();
+        updateData(entity, obj);
+        userRepository.save(entity);
+        return entity;
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
     }
 }
