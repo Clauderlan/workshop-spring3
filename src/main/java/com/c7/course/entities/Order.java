47b09f45f6fffd11ab84cf40a3c574dad71fa99a
Clauderlan <clauderlanbatista@hotmail.com>
2023-04-30 13:23:11 -0300

    Subtotal & Total methods

--- src/main/java/com/c7/course/entities/Order.java
+++ src/main/java/com/c7/course/entities/Order.java
@@ -79,6 +79,10 @@ public class Order implements Serializable {
     }
     public Set<OrderItem> getItems() {return items;}
 
+    public Double getTotal(){
+        return items.stream().map(x -> x.getSubTotal()).reduce(0.0, (x, y) -> x + y);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
