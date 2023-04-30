8d80e494e0fe35d7f14d6dfd8ca66afe2012773c
Clauderlan <clauderlanbatista@hotmail.com>
2023-04-30 14:54:44 -0300

    Exception handling - delete

--- src/main/java/com/c7/course/services/UserService.java
+++ src/main/java/com/c7/course/services/UserService.java
@@ -2,8 +2,11 @@ package com.c7.course.services;
 
 import com.c7.course.entities.User;
 import com.c7.course.repositories.UserRepository;
+import com.c7.course.services.exceptions.DatabaseException;
 import com.c7.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.parser.Entity;
@@ -29,7 +32,13 @@ public class UserService {
         return userRepository.save(obj);
     }
     public void delete(Long id){
-        userRepository.deleteById(id);
+        try{
+            userRepository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
     public User update(Long id, User obj){
         User entity = userRepository.findById(id).get();
---
+++ src/main/java/com/c7/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,7 @@
+package com.c7.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+    public DatabaseException(String e){
+        super(e);
+    }
+}
